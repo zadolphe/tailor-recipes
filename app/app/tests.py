54692ff224c0b68b6cfed5dd7a5a7c2e546fab27
Zach <zach.adolphe@gmail.com>
2022-08-31 15:54:23 -0600

    added psycop2 for connecting postgres to django

---
+++ app/app/tests.py
@@ -0,0 +1,23 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc 
+
+class CalcTests(SimpleTestCase):
+    "Test the calc module "
+
+    def test_add_numbers(self):
+        "test adding numbers together"
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11) 
+
+    def test_subtract_numbers(self):
+        "subtracting numbers from each other"
+        sub_result = calc.subtract(6,4)
+
+        #write test first 
+        self.assertEqual(sub_result, 2)
